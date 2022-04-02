ed0a6633dc4001d9c91befce4cd13a634519070a
phuoc <thaiphuoc1303@gmail.com>
2022-04-02 13:47:39 +0700

    2/4

--- app/src/main/java/com/example/doantest/DemoTestActivity.java
+++ app/src/main/java/com/example/doantest/DemoTestActivity.java
@@ -1,14 +1,29 @@
 package com.example.doantest;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class DemoTestActivity extends AppCompatActivity {
-
+    ImageButton btnCrop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_test);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        init();
+        btnCrop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+    void init(){
+        btnCrop = (ImageButton) findViewById(R.id.btnCrop);
     }
 }
